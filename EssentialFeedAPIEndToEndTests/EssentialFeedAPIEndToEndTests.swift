819f2d69d65934d828a24656b802e59c28aa6824
Matias Glessi <matiasglessi@gmail.com>
2023-02-06 00:20:34 -0300

    Extract test setup to helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,19 +11,7 @@ import EssentialFeed
 class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
-        
-        let expectation = expectation(description: "Wait for completion")
-        var recievedResult: LoadFeedResult?
-        loader.load { result in
-            recievedResult = result
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 5.0)
-        
-        switch recievedResult {
+        switch getFeedResult() {
         case let .success(recievedItems):
             XCTAssertEqual(recievedItems.count, 8, "Expected 8 items in the test account feed.")
             XCTAssertEqual(recievedItems[0], expectedItem(at: 0))
@@ -41,6 +29,22 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
         default:
             XCTFail("Expectedm successful feed result, got no result instead.")
         }
+    }
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let expectation = expectation(description: "Wait for completion")
+        var recievedResult: LoadFeedResult?
+        loader.load { result in
+            recievedResult = result
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 5.0)
+        
+        return recievedResult
     }
     
     // MARK: - Helpers
